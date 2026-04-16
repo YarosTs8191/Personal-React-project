4e2aad7d3fdd98672d138c01f0ef46c990b89678
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2026-04-16 12:04:00 +0200

    complet-gallery-projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,16 @@
 import styles from "./Projects.module.css";
+import { ColumnsPhotoAlbum } from "react-photo-album";
+import "react-photo-album/columns.css";
+
+import { projects } from "../../data/projects";
+
+import { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
 function Projects() {
+  const [index, setIndex] = useState(-1);
+
   return (
     <section className={styles.projects} id="projects">
       <div className="container">
@@ -18,8 +28,19 @@ function Projects() {
         </div>
 
         <div className={styles.gallery}>
-          {/* project images will go here */}
+          <ColumnsPhotoAlbum
+            photos={projects}
+            columns={3}
+            onClick={({ index }) => setIndex(index)}
+          />
         </div>
+
+        <Lightbox
+          open={index >= 0}
+          close={() => setIndex(-1)}
+          index={index}
+          slides={projects}
+        />
       </div>
     </section>
   );
--- src/data/projects.js
+++ src/data/projects.js
@@ -2,15 +2,15 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
-import project4 from "../assets/projects/project-5.jpg";
-import project4 from "../assets/projects/project-6.jpg";
-import project4 from "../assets/projects/project-7.jpg";
-import project4 from "../assets/projects/project-8.jpg";
-import project4 from "../assets/projects/project-9.jpg";
-import project4 from "../assets/projects/project-10.jpg";
-import project4 from "../assets/projects/project-11.jpg";
-import project4 from "../assets/projects/project-12.jpg";
-import project4 from "../assets/projects/project-13.jpg";
+import project5 from "../assets/projects/project-5.jpg";
+import project6 from "../assets/projects/project-6.jpg";
+import project7 from "../assets/projects/project-7.jpg";
+import project8 from "../assets/projects/project-8.jpg";
+import project9 from "../assets/projects/project-9.jpg";
+import project10 from "../assets/projects/project-10.jpg";
+import project11 from "../assets/projects/project-11.jpg";
+import project12 from "../assets/projects/project-12.jpg";
+import project13 from "../assets/projects/project-13.jpg";
 
 export const projects = [
   {
